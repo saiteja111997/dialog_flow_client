426b9ea0607616937d11b69919e649c9662c88dc
teja <saiteja.chippagiri@jukshio.com>
2023-03-21 18:08:56 +0530

    Working code : Taking audio from microphone and injecting output into the browser

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React, { useState, useRef } from 'react';
 
 const RecordAudio = () => {
   const [recording, setRecording] = useState(false);
-  const [audioURL, setAudioURL] = useState(null);
+  // const [audioURL, setAudioURL] = useState(null);
   const recorderRef = useRef(null);
   const [agentResponse, setAgentResponse] = useState("");
+  // const [audioResponse, setAudioResponse] = useState("");
 
   const handleRecordClick = async () => {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -21,11 +22,11 @@ const RecordAudio = () => {
       recorderRef.current.stop();
       recorderRef.current.ondataavailable = async (e) => {
         const audioBlob = new Blob([e.data], { type: 'audio/webm' });
-        setAudioURL(URL.createObjectURL(audioBlob));
+        // setAudioURL(URL.createObjectURL(audioBlob));
   
         const formData = new FormData();
         formData.append('audio', audioBlob, 'recording.webm');
-        const url = 'http://localhost:8000/getDialogFlowResponseWithAudioInput'; // Use http:// instead of localhost:
+        const url = 'http://localhost:8000/getDialogFlowResponseWithAudioOutput'; // Use http:// instead of localhost:
         console.log("Sending request with an audio recording!!")
         const response = await fetch(url, {
           method: 'POST',
@@ -35,6 +36,15 @@ const RecordAudio = () => {
           const responseBody = await response.json();
           console.log(responseBody);
           setAgentResponse(responseBody.agentResponse);
+          const audioResponse = responseBody.audioResponse
+
+          sessionStorage.setItem("key", "value");
+
+          const uint8Array = new Uint8Array(audioResponse.data);
+          const arrayBuffer = uint8Array.buffer;
+          playOutput(arrayBuffer)
+          console.log("Type : " ,typeof arrayBuffer)
+          console.log("Length : ", arrayBuffer.byteLength)
         } else {
           console.error(`HTTP error: ${response.status}`);
         }
@@ -44,20 +54,46 @@ const RecordAudio = () => {
       console.error(error);
     }
   };
+
+  function playOutput(arrayBuffer){
+    // console.log("Entered")
+    let audioContext = new AudioContext();
+    let outputSource;
+    try {
+        if(arrayBuffer.byteLength > 0){
+            console.log("Audio Detected!!")
+            // 2)
+            audioContext.decodeAudioData(arrayBuffer,
+            function(buffer){
+                // 3)
+                audioContext.resume();
+                outputSource = audioContext.createBufferSource();
+                outputSource.connect(audioContext.destination);
+                outputSource.buffer = buffer;
+                outputSource.start(0);
+            },
+            function(){
+                console.log(arguments);
+            });
+        }
+    } catch(e) {
+        console.log(e);
+    }
+   }
   
 
   return (
     <div>
-      {!recording && !audioURL && (
+      {!recording && (
         <button onClick={handleRecordClick}>Record</button>
       )}
       {recording && <button onClick={handleStopClick}>Stop</button>}
-      {audioURL && agentResponse && (
+      {agentResponse && (
         <div>
         <p>{agentResponse}</p> {/* this will display the transcription text */}
-        <audio controls>
+        {/* <audio controls>
           <source src={audioURL} type="audio/webm" />
-        </audio>
+        </audio> */}
       </div>
       )}
     </div>
